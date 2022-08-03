60795d2637f2d3bebd7ddb63ef0e36af61c3a3be
jcastro97 <jcastro@crnova.com>
2022-08-02 18:07:45 -0600

    Dynamic components conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -2,55 +2,70 @@
   <div>
     <!-- <the-header></the-header> -->
     <TheHeader />
-    <!--     <badge-list></badge-list> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!--     <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <component :is="selectedComponent"></component>
+    <!-- <badge-list></badge-list>
     <BadgeList />
-    <!--     <user-info
+        <user-info
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info> -->
+    ></user-info>
     <UserInfo
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
     />
-    <!-- <CourseGoals>
+    <CourseGoals>
       <template #default="props">
         <h2>{{ props.goal }}</h2>
         <p>{{ props["anotherProp"] }}</p>
       </template>
-    </CourseGoals> -->
-    <!-- If you're only targetting only the default slot 
-You can remove the unnecesary template wrapper -->
+    </CourseGoals>
+    If you're only targetting only the default slot 
+You can remove the unnecesary template wrapper
     <CourseGoals #default="props">
       <h2>{{ props.goal }}</h2>
       <p>{{ props["anotherProp"] }}</p>
-    </CourseGoals>
+    </CourseGoals> -->
   </div>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
-import UserInfo from "./components/UserInfo.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+/* import UserInfo from "./components/UserInfo.vue";
 import BadgeList from "./components/BadgeList.vue";
-import CourseGoals from "./components/CourseGoals.vue";
+import CourseGoals from "./components/CourseGoals.vue"; */
 export default {
   components: {
-    /* "the-header": TheHeader, */
-    /* TheHeader: TheHeader, */
-    TheHeader,
+    /* "the-header": TheHeader,
+    TheHeader: TheHeader,
     UserInfo,
     BadgeList,
-    CourseGoals,
+    CourseGoals, */
+    TheHeader,
+    ManageGoals,
+    ActiveGoals,
   },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
