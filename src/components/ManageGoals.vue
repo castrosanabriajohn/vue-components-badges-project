c8754fcfd0f79c7dd540df4e8e4a1e9c4f4dda5b
jcastro97 <jcastro@crnova.com>
2022-08-02 18:56:07 -0600

    fragments

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,16 +1,15 @@
 <template>
-  <div>
-    <h2>Manage Goals</h2>
-    <input type="text" ref="goal" />
-    <button @click="addGoal">Add Goal</button>
-    <teleport to="body">
-      <error-alert v-if="invalidInput">
-        <h2>Input is invalid!</h2>
-        <p>Please enter at least one character...</p>
-        <button @click="dismissError">Ok</button>
-      </error-alert>
-    </teleport>
-  </div>
+  <!-- In vue3 you can have multiple top level elements -->
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal" />
+  <button @click="addGoal">Add Goal</button>
+  <teleport to="body">
+    <error-alert v-if="invalidInput">
+      <h2>Input is invalid!</h2>
+      <p>Please enter at least one character...</p>
+      <button @click="dismissError">Ok</button>
+    </error-alert>
+  </teleport>
 </template>
 
 <script>
