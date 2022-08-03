ba5836af81027a5084e933f05536911862ecfbba
jcastro97 <jcastro@crnova.com>
2022-08-02 18:42:23 -0600

    keep-alive show modal

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,3 +1,35 @@
 <template>
-  <h2>Manage Goals</h2>
+  <div>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal" />
+    <button @click="addGoal">Add Goal</button>
+    <error-alert v-if="invalidInput">
+      <h2>Input is invalid!</h2>
+      <p>Please enter at least one character...</p>
+      <button @click="dismissError">Ok</button>
+    </error-alert>
+  </div>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: { ErrorAlert },
+  data() {
+    return {
+      invalidInput: false,
+    };
+  },
+  methods: {
+    addGoal() {
+      const inputValue = this.$refs.goal.value;
+      if (inputValue == "") {
+        this.invalidInput = true;
+      }
+    },
+    dismissError() {
+      this.invalidInput = false;
+    },
+  },
+};
+</script>
