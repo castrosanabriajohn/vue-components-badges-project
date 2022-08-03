7342293fb0f801e97d4970040e0772f6fec8301b
jcastro97 <jcastro@crnova.com>
2022-08-02 18:47:53 -0600

    teleport

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -3,11 +3,13 @@
     <h2>Manage Goals</h2>
     <input type="text" ref="goal" />
     <button @click="addGoal">Add Goal</button>
-    <error-alert v-if="invalidInput">
-      <h2>Input is invalid!</h2>
-      <p>Please enter at least one character...</p>
-      <button @click="dismissError">Ok</button>
-    </error-alert>
+    <teleport to="body">
+      <error-alert v-if="invalidInput">
+        <h2>Input is invalid!</h2>
+        <p>Please enter at least one character...</p>
+        <button @click="dismissError">Ok</button>
+      </error-alert>
+    </teleport>
   </div>
 </template>
 
